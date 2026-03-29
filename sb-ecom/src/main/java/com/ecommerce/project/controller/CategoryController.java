3330c24d3373206c82776c1b99e86f4fc32bfd68
Vedant <vedantdumbre0706@gmail.com>
2026-03-30 00:48:41 +0530

    ResponseEntity for all Endpoints

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -19,18 +19,20 @@ public class CategoryController {
 
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories() { // made a get request for all categories
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<Category>> getAllCategories() { // made a get request for all categories
+        List<Category> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @PostMapping("/api/public/categories")
-    public String createCategory(@RequestBody Category category){  // made a post request for creating new categories
+    public ResponseEntity<String> createCategory(@RequestBody Category category){  // made a post request for creating new categories
         categoryService.createCategory(category);  // we are adding a category in a JSON format and testing it in postman
-        return "Category created successfully";
+        return new ResponseEntity<>("Category created successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+        // making a try catch block for handling the exception and giving personalized message
         try {
             String status = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
